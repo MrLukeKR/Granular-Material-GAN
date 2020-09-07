f9561f7ebeda0dca79e68b97389d4dd181327bba
Luke Rose <37333663+MrLukeKR@users.noreply.github.com>
2020-09-07 12:43:22 +0100

    Added BoneJ particle analysis and Scale setting to ImageJ analysis script

--- ImageJAnalysis/AnalyseCore.py
+++ ImageJAnalysis/AnalyseCore.py
@@ -5,10 +5,11 @@ from ij import IJ as ij
 from os import listdir
 from os.path import isdir, join
 
-base_directory = "D:/OneDrive - The University of Nottingham/Research/Results/";
+base_directory = "/run/media/***REMOVED***/Experiments/Doctorate/Phase1/experiments/Results/Figures/Aggregate-CT-Scans/";
 results_dirs = [f + '/' for f in listdir(base_directory) if isdir(join(base_directory, f))]
 
 def analyseCore(results_dir):
+	ij.run("Set Scale...", "distance=1 known=0.096 unit=mm global")
 	imp = ij.getImage()
 
 	ij.log("Cropping to ROI...");
@@ -26,17 +27,19 @@ def analyseCore(results_dir):
 	ij.run("Convert to Mask", "method=Default background=Light calculate");
 	ij.run("Invert LUT");
 
-	ij.log("Removing Small Particles...");
-	ij.run("Dilate", "stack");
-	ij.run("Erode", "stack");
+	ij.log("Analysing Particles for Void Volume, Average Diameter and Euler Characteristic...")
+	ij.run("Particle Analyser", "enclosed_volume euler thickness min=0.000 max=Infinity surface_resampling=2 surface=Gradient split=0.000 volume_resampling=2");
+	ij.saveAs("Results", results_dir + "AnalyseParticles_Results.csv");
+
+	raise Exception()
 
 	ij.log("Converting to Skeleton...");
 	ij.run("Skeletonize (2D/3D)");
-	
+
 	ij.log("Analysing Skeleton...");
 	ij.run("Analyze Skeleton (2D/3D)", "prune=none show");
 
-	ij.saveAs("Results", results_dir + "AnalyseSkeleton_Results.csv");	
+	ij.saveAs("Results", results_dir + "AnalyseSkeleton_Results.csv");
 
 	for new_dir in ["/Skeleton/", "/Skeleton/Tagged/", "/Skeleton/Original/"]:
 		if not os.path.exists(results_dir + new_dir):
@@ -52,7 +55,7 @@ for results_dir in results_dirs:
 		ij.log(results_dir)
 		base_result_dir = base_directory + results_dir
 		fold_dirs = [f + '/' for f in listdir(base_result_dir) if isdir(join(base_result_dir, f))]
-		
+
 		for fold_dir in fold_dirs:
 			if(fold_dir.startswith("Fold")):
 				ij.log(fold_dir)
@@ -61,11 +64,11 @@ for results_dir in results_dirs:
 				core_slice_dir = core_dir + core[0] + "CoreSlices_PostProcessed/"
 
 				analysis_dir = core_dir + "/Analysis/"
-				
+
 				if not os.path.exists(analysis_dir):
 					os.makedirs(analysis_dir)
-        		
+
 				ij.log(core_slice_dir)
-				ij.run("Image Sequence...", "select=[" + core_slice_dir + "] dir=[" + core_slice_dir + "] sort")
+				ij.run("Image Sequence...", "open=[" + core_slice_dir + "] sort")
 				analyseCore(analysis_dir)
 				ij.run("Close All")
